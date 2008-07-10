e571257391817ad107bb53f8436c4665935e42b2
peter_bulychev <peter_bulychev@7cf15c16-0848-0410-9509-a110975daa34>
2008-07-10 09:38:39 +0000

    Commited changes from Jeroen Vloothuis removed --recursive, added --no-recursion

--- clonedigger/clonedigger.py
+++ clonedigger/clonedigger.py
@@ -23,119 +23,135 @@ if __name__ == '__main__':
 
 import re
 import os
-import getopt
 import traceback
-import pdb
+from optparse import OptionParser
+from fnmatch import fnmatch
 
 import ast_suppliers
 import clone_detection_algorithm
 import arguments 
 import html_report
 
-help_string = """To run Clone Digger type:
+def main():
+    cmdline = OptionParser(usage="""To run Clone Digger type:
 python clonedigger.py [OPTION]... [SOURCE FILE OR DIRECTORY]...
 
 The typical usage is:
 python clonedigger.py source_file_1 source_file_2 ...
   or
-python clonedigger.py --recursive path_to_source_tree
+python clonedigger.py path_to_source_tree
 Don't forget to remove automatically generated sources, tests and third party libraries from the source tree.
 
-Options:
+Notice:
 The semantics of threshold options is discussed in the paper "Duplicate code detection using anti-unification", which can be downloaded from the site http://clonedigger.sourceforge.net . All arguments are optional. Supported options are: 
---language=LANGUAGE , the programming language of source files searched for clones. Supported values are 'python'(by default) and 'java'. Warning: Clone Digger could not recognize the language of parsed sources automatically.
---recursive , traverse directories recursively. 
---output=FILE NAME , the name of the output file ('output.html' by default). 
---size-threshold=THRESHOLD, the minimum clone size. The clone size for its turn is equal to the count of lines of code in its the largest fragment.
---distance-threshold=THRESHOLD, the maximum amount of differences between pair of sequences in clone pair (5 by default). Larger value leads to larger amount of false positives.
---fast, find only clones, which differ in variable and function names and constants.
---hashing-depth=DEPTH, default value if 1, read the paper for semantics. Compuation can be speed up by increasing increasing this value (but some clones can be list).
---clustering-threshold=THRESHOLD, read the paper for semantics.
---clusterize-using-dcup, mark each statement with its D-cup value instead of the most similar pattern. This option together with --hashing-depth=0 make it possible to catch all considered clones (but it is slow and applicable only to small programs).
-"""
+""")
+    cmdline.add_option('-l', '--language', dest='language',
+                       type='choice', choices=['python', 'java'],
+                       help='the programming language')
+    cmdline.add_option('--no-recursion', dest='no_recursion',
+                       action='store_true', 
+                       help='do not traverse directions recursively')    
+    cmdline.add_option('-o', '--output', dest='output',
+                       help='the name of the output file ("output.html" by default)')
+    cmdline.add_option('--clustering-threshold', 
+                       type='int', dest='clustering_threshold',
+                       help='read the paper for semantics')
+    cmdline.add_option('--distance-threshold', 
+                       type='int', dest='distance_threshold',
+                       help='the maximum amount of differences between pair of sequences in clone pair (5 by default). Larger value leads to larger amount of false positives')
+    cmdline.add_option('--hashing-depth', 
+                       dest='hashing_depth',
+                       help='default value if 1, read the paper for semantics. Compuation can be speed up by increasing increasing this value (but some clones can be list)')
+    cmdline.add_option('--size-threshold', 
+                       type='int', dest='size_threshold',
+                       help='the minimum clone size. The clone size for its turn is equal to the count of lines of code in its the largest fragment')
+    cmdline.add_option('--clusterize-using-dcup', 
+                       action='store_true', dest='clusterize_using_dcup',
+                       help='mark each statement with its D-cup value instead of the most similar pattern. This option together with --hashing-depth=0 make it possible to catch all considered clones (but it is slow and applicable only to small programs)')
+    cmdline.add_option('--dont-print-time', 
+                       action='store_false', dest='print_time',
+                       help='do not print time')
+    cmdline.add_option('-f', '--force', 
+                       action='store_true', dest='force',
+                       help='')
+    cmdline.add_option('--force-diff', 
+                       action='store_true', dest='use_diff',
+                       help='')
+    cmdline.add_option('--fast', 
+                       action='store_true', dest='clusterize_using_hash',
+                       help='find only clones, which differ in variable and function names and constants')
+    cmdline.add_option('--ignore-dir', 
+                       action='append', dest='ignore_dirs',
+                       help='exclude directories from parsing')
 
-def main():
-    output_file_name = 'output.html'
-    optlist, source_file_names = getopt.getopt(sys.argv[1:], '', ['language=', 'output=', 'clustering-threshold=', 'distance-threshold=', 'hashing-depth=', 'size-threshold=', 'clusterize-using-dcup', 'recursive', 'report-statement-marks', 'help', 'dont-print-time', 'force', 'force-diff', 'fast', 'links-for-eclipse'])
+    cmdline.set_defaults(output='output.html',
+                         language='python', 
+                         ingore_dirs=[],
+                         **arguments.__dict__)
+
+    (options, source_file_names) = cmdline.parse_args()
     source_files = [] 
-    #TODO remove in release
-    supplier = ast_suppliers.abstract_syntax_tree_suppliers['python']
     report = html_report.HTMLReport()    
-    recursive = False 
-    if ('--help', '') in optlist:
-		print help_string
-		sys.exit(0)
 
-    for (parameter, value) in optlist:
-	if parameter == '--language':
-	    assert(abstract_syntax_tree_suppliers.has_key(value))
-	    supplier = abstract_syntax_tree_suppliers[value]
-	    if value == 'java':
-		arguments.use_diff = True
+    supplier = ast_suppliers.abstract_syntax_tree_suppliers[options.language]
+    if options.language == 'java':
+        options.use_diff = True
+
+    output_file_name = options.output
+
+    for option in cmdline.option_list:
+        if option.dest is None:
+            continue
+        setattr(arguments, option.dest, getattr(options, option.dest))
+
+    if options.distance_threshold is None:
+        arguments.distance_threshold = supplier.distance_threshold
+    if options.size_threshold is None:
+        arguments.size_threshold = supplier.size_threshold
     
-    arguments.size_threshold = supplier.size_threshold
-    arguments.distance_threshold = supplier.distance_threshold
-
-    for (parameter, value) in optlist:
-	if parameter == '--output':
-	    output_file_name = value
-	elif parameter == '--clustering-threshold':
-	    arguments.clustering_threshold = int(value)
-	elif parameter == '--distance-threshold':
-	    arguments.distance_threshold = int(value)
-	elif parameter == '--size-threshold':
-	    arguments.size_threshold = int(value)
-	elif parameter == '--hashing-depth':
-	    arguments.hashing_depth = int(value)
-	elif parameter == '--clusterize-using-dcup':
-	    arguments.clusterize_using_dcup = True
-	elif parameter == '--recursive':
-	    recursive = True
-	elif parameter == '--report-statement-marks':
-	    antiunification.report_unifiers = True
-	elif parameter == '--dont-print-time':
-	    arguments.print_time = False 
-	elif parameter == '--force':
-	    arguments.force = True
-	elif parameter == '--force-diff':
-	    arguments.use_diff = True
-	elif parameter == '--fast':
-	    arguments.clusterize_using_hash = True
-	elif parameter == '--links-for-eclipse':
-	    arguments.links_for_eclipse = True
     report.startTimer('Construction of AST')
+
+    def parse_file(file_name):
+        try:
+            print 'Parsing ', file_name, '...',
+            sys.stdout.flush()
+            source_file = supplier(file_name)
+            source_file.getTree().propagateCoveredLineNumbers()
+            source_files.append(source_file)
+            report.addFileName(file_name)                
+            print 'done'
+        except:
+            s = 'Error: can\'t parse "%s" \n: '%(file_name,) + traceback.format_exc()
+            report.addErrorInformation(s)
+            print s
+
+    def walk(dirname):
+        for dirpath, dirs, files in os.walk(file_name):
+            dirs[:] = (not options.ignore_dirs and dirs)  or [d for d in dirs if d not in options.ignore_dirs]
+            # Skip all non-parseable files
+            files[:] = [f for f in files 
+                        if os.path.splitext(f)[1][1:] == supplier.extension]
+            yield (dirpath, dirs, files)
+
     for file_name in source_file_names:
-	def parse_file(file_name):
-	    try:
-		print 'Parsing ', file_name, '...',
-		sys.stdout.flush()
-		source_file = supplier(file_name)
-		source_file.getTree().propagateCoveredLineNumbers()
-		source_files.append(source_file)
-		report.addFileName(file_name)		
-		print 'done'
-	    except:
-		s = 'Error: can\'t parse "%s" \n: '%(file_name,) + traceback.format_exc()
-		report.addErrorInformation(s)
-		print s
-	if os.path.isdir(file_name):
-	    def parse_dir(root, files):
-		for rel_file_name in files:
-		    if re.search('\.%s$'%(supplier.extension,), rel_file_name):
-			full_file_name = os.path.join(root, rel_file_name)
-			parse_file(full_file_name)
-	    if recursive:
-		for root, dir, files in os.walk(file_name):
-		    parse_dir(root, files)
+        if os.path.isdir(file_name):
+	    if arguments.no_recursion:
+		dirpath = file_name
+		files = [os.path.join(file_name, f) for f in os.listdir(file_name) 
+                        if os.path.splitext(f)[1][1:] == supplier.extension]
+		for f in files:
+		    parse_file(f)
 	    else:
-		parse_dir(file_name, os.listdir(file_name))
-	else:
-	    parse_file(file_name)
-	
+		for dirpath, dirnames, filenames in walk(file_name):
+		    for f in filenames:
+			parse_file(os.path.join(dirpath, f))
+        else:
+            parse_file(file_name)
+        
     report.stopTimer()
     duplicates = clone_detection_algorithm.findDuplicateCode(source_files, report)
     for duplicate in duplicates:
-	report.addClone(duplicate)
+        report.addClone(duplicate)
     report.sortByCloneSize()
     report.writeReport(output_file_name)
 
