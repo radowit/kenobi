5e3992aeae120b07230771794192e6d0f37dd5ac
zzaabb <zzaabb@7cf15c16-0848-0410-9509-a110975daa34>
2008-07-24 09:58:09 +0000

    removed asserts ; ;

--- org.clonedigger/runclonedigger.py
+++ org.clonedigger/runclonedigger.py
@@ -6,8 +6,6 @@ try:
 except ImportError:
 #    os.system('python ./setup.py easy_install --install-dir . clonedigger')
     python_path = os.environ['PYTHONPATH']
-    assert(';' not in python_path)
-    assert(':' not in python_path)
     sys.argv = [sys.argv[0], 'easy_install', '--install-dir',python_path, 'clonedigger']
     import setup
     sys.exit(143)
