2b8b8a54b6865a1ab2a796a541375c2821dfc6f0
zzaabb <zzaabb@7cf15c16-0848-0410-9509-a110975daa34>
2008-07-24 10:10:14 +0000

    os chdir

--- org.clonedigger/runclonedigger.py
+++ org.clonedigger/runclonedigger.py
@@ -6,6 +6,7 @@ try:
 except ImportError:
 #    os.system('python ./setup.py easy_install --install-dir . clonedigger')
     python_path = os.environ['PYTHONPATH']
+    os.chdir(python_path)
     print 'Missing Clone Digger'
     print 'We will try now to install it to local directory', python_path
     print 'please wait...'
