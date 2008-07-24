c42288d3065558aed9c7c7651462da69cdb15326
zzaabb <zzaabb@7cf15c16-0848-0410-9509-a110975daa34>
2008-07-24 10:02:51 +0000

    warning message

--- org.clonedigger/runclonedigger.py
+++ org.clonedigger/runclonedigger.py
@@ -6,6 +6,9 @@ try:
 except ImportError:
 #    os.system('python ./setup.py easy_install --install-dir . clonedigger')
     python_path = os.environ['PYTHONPATH']
+    print 'Missing Clone Digger'
+    print 'We will try now to install it to local directory', python_path
+    print 'please wait...'
     sys.argv = [sys.argv[0], 'easy_install', '--install-dir',python_path, 'clonedigger']
     import setup
     sys.exit(143)
