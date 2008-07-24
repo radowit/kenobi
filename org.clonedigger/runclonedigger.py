a53b872dd33550fee4b224f339630d8e44d760ee
zzaabb <zzaabb@7cf15c16-0848-0410-9509-a110975daa34>
2008-07-24 08:27:42 +0000

    install clone digger if necessary

--- org.clonedigger/runclonedigger.py
+++ org.clonedigger/runclonedigger.py
@@ -1,2 +1,11 @@
-import clonedigger.clonedigger
+#!/usr/bin/python
+import sys
+import os
+try:
+    import clonedigger.clonedigger
+except ImportError:
+#    os.system('python ./setup.py easy_install --install-dir . clonedigger')
+    sys.argv = [sys.argv[0], 'easy_install', '--install-dir','.', 'clonedigger']
+    import setup
+    sys.exit(143)
 clonedigger.clonedigger.main()
